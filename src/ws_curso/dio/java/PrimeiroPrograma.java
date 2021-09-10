b52a0ae1b3d51eff4a07342da78c0a10de93ae05
Lucas <lucas-silva-santana@hotmail.com>
2021-09-09 21:43:32 -0300

    first commit

---
+++ src/ws_curso/dio/java/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package ws_curso.dio.java;
+
+import ws_curso.dio.java.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));*/
+				
+	}
+	
+}
